ac986f6422fc42f33bf05847cd8c304513d15ff6
Doaa Maher <52929402+doaamaher98@users.noreply.github.com>
2021-11-11 04:11:16 +0200

    UPDATE

--- 00-LifeSign _AVR32/02-MCAL/02-ADC/ADC.c
+++ 00-LifeSign _AVR32/02-MCAL/02-ADC/ADC.c
@@ -1,1 +1,63 @@
-/****************** LifeSign **********************/
+/***************** LifeSign *******************/
+#include "BIT_MATH.h"
+#include "STD_LIB.h"
+
+#include "ADC_config.h"
+#include "ADC_interface.h"
+#include "ADC_private.h"
+
+void void_MCAL_ADC_INIT(void)
+{
+	/*************************************/
+	/*  1- Selecting the ADC Mode    	 */
+	ADMUX 	&= (ADC_VOLTAGE_REF_clr_msk);
+	ADMUX	|= (ADC_VOLTAGE_REF_SELECTOR_msk);
+
+	#if (ADC_MODE_SELECTOR == ADC_MODE_AUTO_TRIGGER)
+		SET_BIT(ADCSRA,5);
+		SFIOR &= (ADC_AUTO_TRIG_SRC_clr_msk);
+		SFIOR |= (ADC_AUTO_TRIG_SRCE_SELECTOR);
+		
+	#elif (ADC_MODE_SELECTOR == ADC_MODE_SINGLE_CONVERSION)
+		CLR_BIT(ADCSRA,5);
+	#endif	
+	/*************************************/
+	/*************************************/
+	
+	/*  2- Selecting the ADC Adjustment  */
+	#if   (ADC_ADJUSTMENT_SELECTOR == ADC_RIGHT_ADJUSTED)
+		CLR_BIT(ADMUX,5);
+	#elif (ADC_ADJUSTMENT_SELECTOR == ADC_LEFT_ADJUSTED)
+		SET_BIT(ADMUX,5);
+	#endif
+	/*************************************/
+	/*************************************/
+	
+	/*  3- Clearing the Interrupt Flag   */
+	SET_BIT(ADCSRA,4);
+
+	/*************************************/
+	/*************************************/
+	
+	/*  4- Enable the ADC     			 */
+	SET_BIT(ADCSRA,7);
+	/*************************************/
+	/*************************************/
+}
+
+void void_MCAL_ADC_StartConvertion(u8 ADC_Channel)
+{
+	/*********** Select ADC Channel **********/
+	ADMUX &= (ADC_CHANNEL_SELECTOR_clr_msk);
+	
+	ADMUX |= (ADC_Channel);
+	
+	/*********** ADC Start Conversion **********/
+	SET_BIT(ADCSRA,6);
+}
+
+u16 u16_MCAL_ADC_GetResult(void)
+{
+	while( GET_BIT(ADCSRA,6) );
+	return ADCLH;
+}
